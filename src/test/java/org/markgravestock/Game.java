70017bbd5a3c5bb0072c07d935b4e3d10adbc0d3
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-22 10:02:17 +0100

    Added first real test. Simplest solution.

---
+++ src/test/java/org/markgravestock/Game.java
@@ -0,0 +1,15 @@
+package org.markgravestock;
+
+public class Game {
+    public boolean hasPlayerWon() {
+        return true;
+    }
+
+    public void playerChoosesRock() {
+
+    }
+
+    public void opponentChoosesScissors() {
+
+    }
+}
---
+++ src/test/java/org/markgravestock/RockBeatsScissorsScenarios.java
@@ -0,0 +1,16 @@
+package org.markgravestock;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class RockBeatsScissorsScenarios {
+    @Test
+    public void given_i_have_chosen_rock_when_the_opponent_chooses_scissors_then_i_should_win() {
+        var game = new Game();
+        game.playerChoosesRock();
+        game.opponentChoosesScissors();
+        assertTrue(game.hasPlayerWon());
+    }
+}
