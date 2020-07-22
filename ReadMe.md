84f1283ce1de4109b15eaa6527faf397b5ad5c48
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-22 10:42:22 +0100

    Refactor test to remove some duplication.

--- ReadMe.md
+++ ReadMe.md
@@ -7,3 +7,4 @@ Using instructions from https://agilekatas.co.uk/katas/RockPaperScissors-Kata
 - When no-one has played is it even a valid question to ask who has won? Should the API allow this?
 - Are players choosing really seperate calls. Normally they both show their move together.
 
+- Does having 2 won methods make API harder to use. How about completedGame.winner().equals(Competitor.PLAYER) or completedGame.isWinner(Competitor.PLAYER) ?
--- src/test/java/org/markgravestock/RockBeatsScissorsScenarios.java
+++ src/test/java/org/markgravestock/RockBeatsScissorsScenarios.java
@@ -1,5 +1,6 @@
 package org.markgravestock;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -7,23 +8,27 @@ import static org.junit.Assert.assertTrue;
 
 public class RockBeatsScissorsScenarios {
 
+    private Game game;
+
+    @Before
+    public void beforeAll() {
+        game = new Game();
+    }
+
     @Test
     public void given_i_have_chosen_rock_when_the_opponent_chooses_scissors_then_i_should_win() {
-        var game = new Game();
         var completedGame = game.playerChoosesRock().opponentChoosesScissors();
         assertTrue(completedGame.hasPlayerWon());
     }
 
     @Test
     public void given_i_have_chosen_rock_when_the_opponent_chooses_scissors_then_they_should_not_win() {
-        var game = new Game();
         var completedGame = game.playerChoosesRock().opponentChoosesScissors();
         assertFalse(completedGame.hasOpponentWon());
     }
 
     @Test
     public void given_i_have_chosen_scissors_when_the_opponent_chooses_rock_then_they_should_win() {
-        var game = new Game();
         var completedGame = game.playerChoosesScissors().opponentChoosesRock();
         assertTrue(completedGame.hasOpponentWon());
     }
