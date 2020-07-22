2b58284478516736d7fc3885ab7669c414f4be35
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-22 09:50:45 +0100

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+### Rock Paper Scissors Kata - Java
+
+Using instructions from https://agilekatas.co.uk/katas/RockPaperScissors-Kata
+
+#### Notes
+
---
+++ src/test/java/GameTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class GameTest {
+    @Test
+    public void my_first_test() {
+        assertTrue(true);
+    }
+}
