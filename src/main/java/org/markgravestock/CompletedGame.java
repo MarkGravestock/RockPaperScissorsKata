ebebe58be45dd03f630a0f5e80e7d88d13ccb31b
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-24 12:06:13 +0100

    Just realised methods did not have similar level of abstraction in terms of use of fields vs parameters. Has this approach reduced cohesion?

--- src/main/java/org/markgravestock/CompletedGame.java
+++ src/main/java/org/markgravestock/CompletedGame.java
@@ -15,14 +15,14 @@ public class CompletedGame {
             return Outcome.DRAW;
         }
 
-        if (hasOpponentWon()) {
+        if (hasOpponentWon(opponentChoice, playerChoice)) {
             return Outcome.OPPONENT_WON;
         }
 
         return Outcome.PLAYER_WON;
     }
 
-    private boolean hasOpponentWon() {
+    private boolean hasOpponentWon(Choice opponentChoice, Choice playerChoice) {
         return opponentChoice.equals(Choice.ROCK) && !playerChoice.equals(Choice.PAPER) || opponentChoice.equals(Choice.PAPER) && playerChoice.equals(Choice.ROCK) || opponentChoice.equals(Choice.SCISSORS) && playerChoice.equals(Choice.PAPER);
     }
 
